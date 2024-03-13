88cf25e46b2a1139fdad929c409724c7d54f0e89
Neoks <neokspp@proton.me>
2024-03-13 03:54:29 -0500

    inicios de generador de numeros aleatorio, automorfismo de los numeros del 0 al 9

--- main.c
+++ main.c
@@ -10,6 +10,10 @@ struct ciudad {
 	int c_area;	//Para almacenar el area de la ciudad
 };
 
+int gen_dunit() {
+	
+
+
 char* lectura_palabra() {
 	unsigned int i = 1;
 
@@ -19,6 +23,7 @@ char* lectura_palabra() {
 	char c;
 	
 	while((c = getchar()) != '\n' && c != EOF) {
+		if(!isalnum(c) && c != '_') { c = '_'; }
 		i++;
 		if(i >= UINT_MAX) { printf("Error: longitud de array insegura\n"); free(palabra); return NULL; }
 
@@ -29,6 +34,7 @@ char* lectura_palabra() {
 	}
 
 	palabra[i] = '\0';
+	printf("%s", palabra);
 
 	return palabra;
 }
@@ -46,16 +52,21 @@ char* depurar_palabra(char* palabra) {
 
 int main() {
 	struct ciudad city;	//Creando la variable ciudad
+
 	printf("Ingresa el area de la ciudad\n");	//Indicando que se ingrese el area
 	scanf("%d", &city.c_area);	//Guardando el area en el miembro de la ciudad
 	printf("Ingresa el nombre de la ciudad\n");	//Indicando que se ingrese el nombre
 	getchar();
 	city.c_nombre = lectura_palabra();
-	printf("%s", city.c_nombre);
+
+
 	char* nombre_archivo = malloc(strlen(city.c_nombre) + 5);
+	if(!nombre_archivo) { printf("Error: No se pudo asignar memoria para el nombre del archivo\n"); free(city.c_nombre); return -1; }
+
 	strcpy(nombre_archivo, depurar_palabra(city.c_nombre));
 	strcat(nombre_archivo, ".map");
-	printf("%s%", nombre_archivo);
+
+	printf("%s", nombre_archivo);
 
 	FILE *archivo = fopen(nombre_archivo, "wb");	//Creando el archivo para guardar la ciudad
 	if(!archivo) { printf("Error: No se pudo crear el archivo\n"); free(city.c_nombre); return -1; }
