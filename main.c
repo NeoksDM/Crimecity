da854109ebc02acccb868c309cc37f2bc522a6ca
Neoks <neokspp@proton.me>
2024-03-12 22:25:36 -0500

    mensaje y la concha

--- main.c
+++ main.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
-//Contadores
-int i = 0;
+struct ciudad {
+	int area;
+};
 
-int main(){
-	printf("Hola, yo, esto es Crimecity, ahora no sabes qué hacer,\n");
-		printf("Pero quieres por ahora, crear algo que genere por si sólo, lo siguiente\n");
-		printf("1. Guarde la información de un área,\n");
-		printf("2. Sea capaz de particionar dicha área en sub áreas no necesariamente iguales,\n");
-		printf("3. Las sub áreas están separadas por la carretera, la cual está seccionada en calles,\n");
-		printf("4. La carretera es un conjunto continuo, abierto\n");
-		printf("Por ahora, sera una funcion para calcular raiz cuadrada y precedida por una de descomposicion de digitos\n");
+int main() {
+	struct ciudad city;
+	printf("Ingresa el area de la ciudad\n");
+	scanf("%d", &city.area);
 
-		int numero;
-		scanf("%d", &numero);
-		int aux = 0;		
-		while( aux != numero){
-			i++;
-			aux = numero - i
-			
-		}
+	FILE *archivo = fopen("ciudad.dat", "wb");
+	
+	fwrite(&city, sizeof(struct ciudad), 1, archivo);
+	fclose(archivo);
+
 	return 0;
 }
