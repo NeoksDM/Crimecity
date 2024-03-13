dc53e0246aeabc3d12500db7c46bb227365cf0c0
Neoks <neokspp@proton.me>
2024-03-13 02:18:25 -0500

    agregado creacion de archivo, posee errores en la creacion del nombre del archivo

--- main.c
+++ main.c
@@ -1,18 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <ctype.h>
+#include <string.h>
+
 
 struct ciudad {
-	int area;
+	char *c_nombre;	//Para almacenar el nombre de la ciudad
+	int c_area;	//Para almacenar el area de la ciudad
 };
 
-int main() {
-	struct ciudad city;
-	printf("Ingresa el area de la ciudad\n");
-	scanf("%d", &city.area);
+char* lectura_palabra() {
+	unsigned int i = 1;
 
-	FILE *archivo = fopen("ciudad.chupalo", "wb");
+	char *palabra = malloc(sizeof(char) * i);
+	if(!palabra) { printf("Error: No se creo el puntero: palabra\n"); free(palabra); return NULL; }
+
+	char c;
 	
-	fwrite(&city, sizeof(struct ciudad), 1, archivo);
-	fclose(archivo);
+	while((c = getchar()) != '\n' && c != EOF) {
+		i++;
+		if(i >= UINT_MAX) { printf("Error: longitud de array insegura\n"); free(palabra); return NULL; }
+
+		palabra = realloc(palabra, sizeof(char) * (i + 1));
+		if(!palabra) { printf("Error: No se creo el puntero: palabra\n");  free(palabra); return NULL; }
+
+		palabra[i - 1] = (char)c;
+	}
+
+	palabra[i] = '\0';
+
+	return palabra;
+}
+
+char* depurar_palabra(char* palabra) {
+	char* c = palabra;
+	while(*c){
+		if(!isalnum(*c) && *c != '_') {
+			*c = '_';
+		}
+		c++;
+	}
+	return palabra;
+}
+
+int main() {
+	struct ciudad city;	//Creando la variable ciudad
+	printf("Ingresa el area de la ciudad\n");	//Indicando que se ingrese el area
+	scanf("%d", &city.c_area);	//Guardando el area en el miembro de la ciudad
+	printf("Ingresa el nombre de la ciudad\n");	//Indicando que se ingrese el nombre
+	getchar();
+	city.c_nombre = lectura_palabra();
+	printf("%s", city.c_nombre);
+	char* nombre_archivo = malloc(strlen(city.c_nombre) + 5);
+	strcpy(nombre_archivo, depurar_palabra(city.c_nombre));
+	strcat(nombre_archivo, ".map");
+	printf("%s%", nombre_archivo);
+
+	FILE *archivo = fopen(nombre_archivo, "wb");	//Creando el archivo para guardar la ciudad
+	if(!archivo) { printf("Error: No se pudo crear el archivo\n"); free(city.c_nombre); return -1; }
+	
+	fwrite(&city, sizeof(struct ciudad), 1, archivo);	//Escribiendo la ciudad en el archivo
+	fclose(archivo);	//Cerrando el archivo
+
+	free(city.c_nombre);
 
 	return 0;
 }
